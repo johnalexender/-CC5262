cceb74cdcf14ce4e7ad0fb4a228a26d5c98d4977
BUHARY_MOHAMED_ASFAQ <134863689+johnalexender@users.noreply.github.com>
2024-03-02 04:19:35 -0800

    Enums, data types, and constants: Here are the required enums, data types, and constants:

--- Movie.java
+++ Movie.java
@@ -1,25 +1,26 @@
-public class Movie {
-    private String title;
-    private String genre;
-    private int duration; //in minutes
+from enum import Enum
 
-    // Constructor
-    public Movie(String title, String genre, int duration) {
-        this.title = title;
-        this.genre = genre;
-        this.duration = duration;
-    }
 
-    // Getters
-    public String getTitle() {
-        return title;
-    }
+class BookingStatus(Enum):
+    REQUESTED, PENDING, CONFIRMED, CHECKED_IN, CANCELED, ABANDONED = 1, 2, 3, 4, 5, 6
 
-    public String getGenre() {
-        return genre;
-    }
 
-    public int getDuration() {
-        return duration;
-    }
-}
+class SeatType(Enum):
+    REGULAR, PREMIUM, ACCESSIBLE, SHIPPED, EMERGENCY_EXIT, OTHER = 1, 2, 3, 4, 5, 6
+
+
+class AccountStatus(Enum):
+    ACTIVE, BLOCKED, BANNED, COMPROMISED, ARCHIVED, UNKNOWN = 1, 2, 3, 4, 5, 6
+
+
+class PaymentStatus(Enum):
+    UNPAID, PENDING, COMPLETED, FILLED, DECLINED, CANCELLED, ABANDONED, SETTLING, SETTLED, REFUNDED = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+
+class Address:
+    def __init__(self, street, city, state, zip_code, country):
+        self.__street_address = street
+        self.__city = city
+        self.__state = state
+        self.__zip_code = zip_code
+        self.__country = country
